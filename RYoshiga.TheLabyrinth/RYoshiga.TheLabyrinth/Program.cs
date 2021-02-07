a4fd757c17723db1a671ae30817661ce528bf9b3
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2021-02-07 16:03:11 +0000

    Second scenario passing with a basic recursive algorithm

--- RYoshiga.TheLabyrinth/RYoshiga.TheLabyrinth/Program.cs
+++ RYoshiga.TheLabyrinth/RYoshiga.TheLabyrinth/Program.cs
@@ -5,16 +5,39 @@ using System.Text;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Point
+public struct Point
 {
-    public Point(int x, int y)
+    public Point(int row, int column)
     {
-        X = x;
-        Y = y;
+        Row = row;
+
+        Column = column;
     }
 
-    public int X { get; }
-    public int Y { get; }
+    public Point(Point current, Direction direction)
+    {
+        Column = current.Column;
+        Row = current.Row;
+
+        switch (direction)
+        {
+            case Direction.LEFT:
+                Column--;
+                break;
+            case Direction.RIGHT:
+                Column++;
+                break;
+            case Direction.DOWN:
+                Row++;
+                break;
+            case Direction.UP:
+                Row--;
+                break;
+        }
+    }
+
+    public int Column { get; }
+    public int Row { get; }
 }
 
 /**
@@ -30,15 +53,13 @@ class Player
         int C = int.Parse(inputs[1]); // number of columns.
         int A = int.Parse(inputs[2]); // number of rounds between the time the alarm countdown is activated and the time the alarm goes off.
 
-
-
-        var map = new Map(R);
+        var map = new Map(R, C);
         // game loop
         while (true)
         {
             inputs = Console.ReadLine().Split(' ');
-            int KR = int.Parse(inputs[0]); // row where Kirk is located.
-            int KC = int.Parse(inputs[1]); // column where Kirk is located.
+            int currentRow = int.Parse(inputs[0]); // row where Kirk is located.
+            int currentColumn = int.Parse(inputs[1]); // column where Kirk is located.
 
             for (int i = 0; i < R; i++)
             {
@@ -46,21 +67,33 @@ class Player
                 map.SetRow(row, i);
             }
 
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
+            Console.Error.WriteLine(map.ToString());
+            Console.Error.WriteLine("END");
 
-            Console.WriteLine(map.DecideWhereToGo(new Point(KR, KC))); // Kirk's next move (UP DOWN LEFT or RIGHT).
+            var command = map.DecideWhereToGo(new Point(currentRow, currentColumn));
+            Console.WriteLine(command);
 
         }
     }
 
     class Map
     {
+        private const char WALL = '#';
         private readonly string[] _rows;
-        private bool controlRoomFound = false;
-        public Map(int rows)
+        private bool _controlRoomFound;
+        private bool _canGoRight;
+        private bool _canGoLeft;
+        private int _maxColumn;
+        private Stack<Point> _positions = new Stack<Point>();
+        private Dictionary<Point, bool> _searchCache;
+
+        public Map(int rows, int column)
         {
+            _maxColumn = column;
             _rows = new string[rows];
+
+            _positions = new Stack<Point>();
+            _searchCache = new Dictionary<Point, bool>();
         }
 
         public void SetRow(string row, int i)
@@ -68,17 +101,141 @@ class Player
             _rows[i] = row;
         }
 
-        public string DecideWhereToGo(Point currentPosition)
+        public string DecideWhereToGo(Point current)
         {
-            if (_rows[currentPosition.X][currentPosition.Y] == 'C')
+            SetFoundControlRoom(current);
+
+            if (_controlRoomFound)
             {
-                controlRoomFound = true;
+                var tryPop = _positions.TryPop(out Point result);
+                if (tryPop)
+                {
+                    var direction = FindDirection(current, result);
+                    return FormatResponse(direction);
+                }
+
+                return "DONT KNOW";
             }
 
-            if (controlRoomFound)
-                return "LEFT";
+            _positions.Push(current);
+            _searchCache = new Dictionary<Point, bool>();
 
-            return "RIGHT";
+
+            _canGoRight = true;
+            _canGoLeft = true;
+
+            var dfsResult = DFS(current, 0);
+
+            return FormatResponse(dfsResult.Direction);
+        }
+
+        private static string FormatResponse(Direction resultDirection)
+        {
+            return resultDirection.ToString().ToUpper();
+        }
+
+        private Direction FindDirection(Point current, Point previous)
+        {
+            if (current.Row > previous.Row)
+                return Direction.UP;
+
+            if (current.Row < previous.Row)
+                return Direction.DOWN;
+
+            if (current.Column > previous.Column)
+                return Direction.LEFT;
+
+            return Direction.RIGHT;
+        }
+
+        private ScoreResult DFS(Point current, int score)
+        {
+            if (_searchCache.ContainsKey(current))
+            {
+                return new ScoreResult(-1, Direction.UP);
+            }
+
+            _searchCache[current] = true;
+            var charAt = At(current.Row, current.Column);
+            if (charAt == WALL)
+                return new ScoreResult(-1, Direction.UP);
+
+            if (charAt == 'C')
+                return new ScoreResult(int.MaxValue, Direction.UP);
+
+            score++;
+            if (charAt == '?')
+            {
+                return new ScoreResult(score, Direction.UP);
+            }
+
+            var results = new ScoreResult[4]
+            {
+                new ScoreResult(DFS(new Point(current, Direction.LEFT), score).Score, Direction.LEFT),
+                new ScoreResult(DFS(new Point(current, Direction.UP), score).Score, Direction.UP),
+                new ScoreResult(DFS(new Point(current, Direction.RIGHT), score).Score, Direction.RIGHT),
+                new ScoreResult(DFS(new Point(current, Direction.DOWN), score).Score, Direction.DOWN)
+            };
+
+            var scoreResult = results.OrderByDescending(x => x.Score).First();
+
+            return new ScoreResult(scoreResult.Score, scoreResult.Direction);
+        }
+
+        private char At(int row, int column)
+        {
+            if (row < 0 || row >= _rows.Length)
+                return WALL;
+
+            if (column < 0 || column >= _maxColumn)
+                return WALL;
+
+            return _rows[row][column];
+        }
+
+        private void SetFoundControlRoom(Point currentPosition)
+        {
+            if (At(currentPosition.Row, currentPosition.Column) == 'C')
+            {
+                _controlRoomFound = true;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join('\n', _rows);
         }
     }
+}
+
+internal class DfsDecision
+{
+    public int Score { get; }
+    public Direction Direction { get; }
+
+    public DfsDecision(int score, Direction direction)
+    {
+        Score = score;
+        Direction = direction;
+    }
+}
+
+internal struct ScoreResult
+{
+    public int Score { get; }
+    public Direction Direction { get; }
+
+    public ScoreResult(int score, Direction direction)
+    {
+        Score = score;
+        Direction = direction;
+    }
+}
+
+public enum Direction
+{
+    LEFT,
+    RIGHT,
+    DOWN,
+    UP,
 }
